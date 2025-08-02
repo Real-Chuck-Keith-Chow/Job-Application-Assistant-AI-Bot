794ee3334244e446a6a69d17bff56d0bdd3db3e7
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-08-02 12:22:19 -0400

    Update gcp_functions.js

--- backend/gcp_functions.js
+++ backend/gcp_functions.js
@@ -1,1 +1,27 @@
+const Answer = require('../database/models/Answer');
+const logger = require('../utils/logger');
 
+const saveAnswer = async (question, answer) => {
+  try {
+    const docId = await Answer.create(question, answer);
+    return { id: docId, question, answer };
+  } catch (error) {
+    logger.error(`Failed to save answer: ${error}`);
+    throw new Error('Failed to save answer to database');
+  }
+};
+
+const findAnswer = async (question, exactMatch = true) => {
+  try {
+    const answers = await Answer.findByQuestion(question, exactMatch);
+    return answers.length > 0 ? answers[0].answer : null;
+  } catch (error) {
+    logger.error(`Failed to find answer: ${error}`);
+    throw new Error('Failed to retrieve answer from database');
+  }
+};
+
+module.exports = {
+  saveAnswer,
+  findAnswer
+};
